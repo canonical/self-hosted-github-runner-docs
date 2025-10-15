62f3bbf9312b902f9160a5000542669572d823dd
florentianayuwono <florentiana.yuwono@canonical.com>
2025-10-15 21:01:05 +0700

    add anchors

--- docs/conf.py
+++ docs/conf.py
@@ -229,7 +229,7 @@ linkcheck_ignore = [
 
 # A regex list of URLs where anchors are ignored by 'make linkcheck'
 
-linkcheck_anchors_ignore_for_url = [r"https://github\.com/.*"]
+linkcheck_anchors_ignore_for_url = [r"https://github\.com/.*", r"https://library\.canonical\.com/.*"]
 
 # give linkcheck multiple tries on failure
 # linkcheck_timeout = 30
